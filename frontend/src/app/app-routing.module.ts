4c307560d46f659a74f99b163f0c9b7b4e897f42
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-10-31 11:31:05 -0300

    :sparkles: feat: aula 04 - criando o módulos e Roteamento com lazy loading

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-export const mainRoutes: Routes = [];
+export const mainRoutes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  {
+    path: 'courses',
+    loadChildren: () =>
+      import('./courses/courses.module').then(m => m.CoursesModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(mainRoutes, { useHash: true })],
