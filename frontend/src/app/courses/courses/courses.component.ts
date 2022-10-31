8b18b3c48f30ea23bc293ba2ddc1afccccf069d0
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-10-31 11:41:06 -0300

    :boom: style: fix eslint errors

--- frontend/src/app/courses/courses/courses.component.ts
+++ frontend/src/app/courses/courses/courses.component.ts
@@ -3,13 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
-export class CoursesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class CoursesComponent {}
