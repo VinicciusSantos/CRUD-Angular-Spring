6c2cf6af0d6bf678acd4d64951fd58610fa6ebb1
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-11-01 08:28:07 -0300

    :sparkles: feat: aula 06 - criando material table para listar cursos

--- frontend/src/app/courses/courses/courses.component.ts
+++ frontend/src/app/courses/courses/courses.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../models/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss'],
 })
-export class CoursesComponent {}
+export class CoursesComponent {
+  courses: Course[] = [{ _id: '1', name: 'Angular', category: 'Front-End' }];
+  displayedColumns: string[] = ['name', 'category'];
+}
